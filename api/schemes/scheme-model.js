848080a5d1512311cc100b2d4c55d37b55171bf3
Joshua Lovins <joshualovins@Joshuas-MBP.lan>
2021-01-21 02:18:14 -0500

    Finished MVP

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,79 @@
-// scheme-model
+const { returning } = require('../../data/db-config.js');
+const db = require('../../data/db-config.js');
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
+
+function find() {
+    return db('schemes')
+}
+
+function findById(id) {
+    const scheme = db('schemes')
+        .where('id', id)
+        .first()
+    if (!scheme) {
+        return Promise.resolve(null)
+    } else {
+        return scheme
+    }
+}
+
+function findSteps(id) {
+    return db('steps')
+        .join('schemes', 'steps.scheme_id', 'schemes.id')
+        .select('steps.id', 'steps.instructions', 'steps.step_number', 'schemes.scheme_name')
+        .where('scheme_id', id)
+        .orderBy('step_number')
+}
+
+function add(scheme) {
+    return db('schemes')
+        .insert(scheme)
+        .then(ids => {
+            return findById(ids[0])
+        })
+}
+
+function update(changes, id) {
+    const scheme_name = changes['scheme_name']
+    return db('schemes')
+        .where('id', id)
+        .update(changes)
+        // Probably there is a better way:
+        .then(() => {
+            return { 'id': id, 'scheme_name': scheme_name }
+        })
+}
+
+function remove(id) {
+    // const deleted = /
+    
+    return db('schemes')
+    .where('id', id)
+    .del()
+    
+    // const toBeRemoved = db('schemes')
+    //     .where('id', id)
+    //     .first()
+    // console.log(toBeRemoved)
+    // if (!toBeRemoved) {
+    //     return Promise.resolve(null)
+    // } else {
+    //     db('schemes')
+    //         .where('id', id)
+    //         .del()
+    // }
+    // return toBeRemoved
+    // if (toBeRemoved['removed']) {
+    //     return toBeRemoved['removed']
+    // } else {
+    //     return Promise.resolve(null)
+    // }
+}
--- api/schemes/scheme-router.js
+++ api/schemes/scheme-router.js
@@ -101,10 +101,18 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
 
+  let toBeRemoved;
+  Schemes.findById(id)
+    .then(toBeRemovedHack => {
+      toBeRemoved = toBeRemovedHack
+    })
+    .catch(err => {
+      console.log({ message: "Could not do it even wrongly" })
+    })
   Schemes.remove(id)
     .then(deleted => {
       if (deleted) {
-        res.json({ removed: deleted });
+        res.json({ removed: toBeRemoved });
       } else {
         res.status(404).json({ message: 'Could not find scheme with given id' });
       }
